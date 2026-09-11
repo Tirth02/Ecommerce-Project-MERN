8078ef52f5c49ed2b150a038924931229fa1d342
Tirth02 <pateltirth0902@gmail.com>
2026-09-10 22:16:26 -0400

    sort functionality added to collection page

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -10,6 +10,7 @@ const Collection = () => {
   const [filterProducts, setFilterProducts] = useState([]);
   const [category,setCategory] = useState([]);
   const [subCategory,setSubCategory] = useState([]);
+  const [sortType, setSortType] = useState('relavent');
 
   const toggleCategory = (e) => {
     if(category.includes(e.target.value))
@@ -50,9 +51,24 @@ const Collection = () => {
     // console.log(products)
   }
 
-  // const sortProduct = () => {
-    
-  // }
+  const sortProduct = () => {
+    let fpCopy = filterProducts.slice();
+
+    switch(sortType)
+    {
+      case 'low-high':
+        setFilterProducts(fpCopy.sort((a,b) => (a.price-b.price)));
+        break;
+      
+      case 'high-low':
+        setFilterProducts(fpCopy.sort((a,b) => (b.price-a.price)))
+        break;
+      
+      default:
+        applyFilter();
+      
+    }
+  }
 
 
   useEffect(() => {
@@ -60,6 +76,9 @@ const Collection = () => {
     applyFilter();
   },[category,subCategory])
 
+  useEffect(() => {
+    sortProduct()
+  },[sortType])
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
 
@@ -106,7 +125,7 @@ const Collection = () => {
         <div className='flex justify-between text-base sm:text-2xl mb-4'>
           <Title text1={'ALL'} text2={'COLLECTIONS'}/>
           {/* Product Sort */}
-          <select className='border-2 border-gray-300 text-sm px-2'>
+          <select onChange={(e) => setSortType(e.target.value)} className='border-2 border-gray-300 text-sm px-2'>
             <option value="relevant">Sort by: Relevant</option>
             <option value="low-high">Sort by: Low to High</option>
             <option value="high-low">Sort by: High to Low</option>
