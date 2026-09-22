134f50221d4ad4d3bcb92972b51b9b0d901659ca
Tirth02 <pateltirth0902@gmail.com>
2026-09-21 22:58:55 -0400

    implemented api for removal,listAll and fetch single product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -61,11 +61,37 @@ const addProduct = async (req, res) => {
 };
 
 // function for list Product
-const listProducts = async (req, res) => {};
+const listProducts = async (req, res) => {
+  try {
+    const products = await productModel.find({});
+    res.json({success:true,products});
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // function for removing product
-const removeProducts = async (req, res) => {};
+const removeProducts = async (req, res) => {
+  try {
+    await productModel.findByIdAndDelete(req.body.id);
+    res.json({success:true,message:"Product removed"});
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 // function for single product info
-const singleProduct = async (req, res) => {};
+const singleProduct = async (req, res) => {
+  try {
+    const { productId } = req.body;
+    const product = await productModel.findById(productId);
+    res.json({success:true,product});
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export { listProducts, removeProducts, addProduct, singleProduct };
